b1214f3b8abf461aab35711a179e3a1462721766
Albert Melikyan <almeliky@c2r14s2.42yerevan.am>
2023-05-22 13:10:28 +0400

    final vers.

--- next_smaller_same_digits.c
+++ next_smaller_same_digits.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 long long next_smaller_number(unsigned long long n){
-    unsigned long long size = 10;
-    unsigned long long result = n;
-    int  *arr;
+    unsigned long long    size = 10;
+    unsigned long long    result = n;
+    int                    iter = 0;
+    int                    *arr;
 
     for (int i = 0; i < size; i++)
     {
@@ -15,7 +15,7 @@ long long next_smaller_number(unsigned long long n){
           size = i + 1;
           break ;
         }
-		size *= 10;
+        size *= 10;
     }
     arr = malloc(sizeof(int) * size);
     for (int i = size - 1; i >= 0; i--)
@@ -23,19 +23,38 @@ long long next_smaller_number(unsigned long long n){
       arr[i] = n % 10;
       n /= 10;
     }
-	for(int i = size - 1; i > 0; i--)
-	{
-		for(int j = i - 1; j >= 0; j--)
-		{
-			if (!(arr[i] == 0 && j == 0) && arr[i] < arr[j])
-			{
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				j = i;
-			}
-		}
-	}
+      iter = size;
+    while (--iter >= 0)
+    {
+      int max = iter;
+      if (iter < size - 1)
+      {
+        for (int j = size - 1; j > iter; j--)
+        {
+			if (arr[j] == 0 && iter == 0)
+				continue ;
+          	if (arr[j] < arr[iter] && ((max != iter) == (arr[j] > arr[max])))
+            max = j;
+        }
+        if (max != iter && arr[max] < arr[iter])
+        {
+          int temp = arr[iter];
+          arr[iter] = arr[max];
+          arr[max] = temp;
+          for (int i = iter + 1; i < size - 1; i++)
+          {
+            if (arr[i] < arr[i + 1])
+            {
+              int swap = arr[i];
+              arr[i] = arr[i + 1];
+              arr[i + 1] = swap;
+              i = iter;
+            }
+          }
+          break ;
+        }
+      }
+    }
     n = result;
     result = 0;
     for(int i = 0; i < size; i++)
@@ -44,12 +63,7 @@ long long next_smaller_number(unsigned long long n){
       result += arr[i];
     }
     free(arr);
-    // if (n == result)
-    //   return (-1);
+    if (n == result)
+      return (-1);
     return (result);
-}
-
-int main()
-{
-	printf("%lld\n", next_smaller_number(907));
 }
